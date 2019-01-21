e63261747d3b5d2c1d58627f035e826735434dc0
jrys <jacek.rys@dataart.com>
2019-01-21 17:49:38 +0100

    Part2 implemented https://vivekc.xyz/peer-to-peer-video-calling-webrtc-for-android-4132fd0ac54

--- app/src/main/java/pl/myosolutions/videostreampilot/MainActivity.java
+++ app/src/main/java/pl/myosolutions/videostreampilot/MainActivity.java
@@ -1,7 +1,9 @@
 package pl.myosolutions.videostreampilot;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import org.webrtc.AudioSource;
 import org.webrtc.AudioTrack;
@@ -10,24 +12,74 @@ import org.webrtc.CameraEnumerator;
 import org.webrtc.DefaultVideoDecoderFactory;
 import org.webrtc.DefaultVideoEncoderFactory;
 import org.webrtc.EglBase;
+import org.webrtc.IceCandidate;
 import org.webrtc.Logging;
 import org.webrtc.MediaConstraints;
+import org.webrtc.MediaStream;
+import org.webrtc.PeerConnection;
 import org.webrtc.PeerConnectionFactory;
+import org.webrtc.SessionDescription;
 import org.webrtc.SurfaceTextureHelper;
 import org.webrtc.SurfaceViewRenderer;
 import org.webrtc.VideoCapturer;
 import org.webrtc.VideoSource;
 import org.webrtc.VideoTrack;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private static final String TAG = "MainActivity";
+    private PeerConnection localPeer, remotePeer;
+    private PeerConnectionFactory peerConnectionFactory;
+    private SurfaceViewRenderer localVideoView;
+    private SurfaceViewRenderer remoteVideoView;
+    private VideoTrack localVideoTrack;
+    private AudioTrack localAudioTrack;
+    private EglBase rootEglBase;
+    private Button start, call, hangup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initViews();
+        initVideos();
+        start();
+
+
+    }
+
+    private void initVideos() {
+        //EGL is rendering API
+        rootEglBase = EglBase.create();
+        //create surface renderer, init it and add the renderer to the track
+        localVideoView.setMirror(true);
+        localVideoView.init(rootEglBase.getEglBaseContext(), null);
+        localVideoView.setZOrderMediaOverlay(true);
+
+        remoteVideoView.setMirror(true);
+        remoteVideoView.init(rootEglBase.getEglBaseContext(), null);
+        remoteVideoView.setZOrderMediaOverlay(true);
+    }
+
+    private void initViews() {
+        start = (Button) findViewById(R.id.start_call);
+        call = (Button) findViewById(R.id.init_call);
+        hangup = (Button) findViewById(R.id.end_call);
+        localVideoView = (SurfaceViewRenderer) findViewById(R.id.local_gl_surface_view);
+        remoteVideoView = (SurfaceViewRenderer) findViewById(R.id.remote_gl_surface_view);
+
+        start.setOnClickListener(this);
+        call.setOnClickListener(this);
+        hangup.setOnClickListener(this);
+    }
+
+    private void start() {
+        start.setEnabled(false);
+        call.setEnabled(true);
         //Initialize PeerConnectionFactory globals.
         //Params are context, initAudio,initVideo and videoCodecHwAcceleration
         //PeerConnectionFactory.initializeAndroidGlobals(this, true, true, true);
@@ -35,8 +87,7 @@ public class MainActivity extends AppCompatActivity {
         PeerConnectionFactory.InitializationOptions initializationOptions = builder.createInitializationOptions();
         PeerConnectionFactory.initialize(initializationOptions);
 
-        //EGL is rendering API
-        EglBase rootEglBase = EglBase.create();
+
 
         //Create a new PeerConnectionFactory instance.
         DefaultVideoEncoderFactory defaultVideoEncoderFactory = new DefaultVideoEncoderFactory(rootEglBase.getEglBaseContext(),  true,  true);
@@ -44,13 +95,12 @@ public class MainActivity extends AppCompatActivity {
 
         PeerConnectionFactory.Options options = new PeerConnectionFactory.Options();
 
-        PeerConnectionFactory peerConnectionFactory =
+        peerConnectionFactory =
                 PeerConnectionFactory.builder()
-                .setVideoEncoderFactory(defaultVideoEncoderFactory)
-                .setVideoDecoderFactory(defaultVideoDecoderFactory)
-                .setOptions(options)
-                .createPeerConnectionFactory()
-                ;
+                        .setVideoEncoderFactory(defaultVideoEncoderFactory)
+                        .setVideoDecoderFactory(defaultVideoDecoderFactory)
+                        .setOptions(options)
+                        .createPeerConnectionFactory();
 
 
         //Now create a VideoCapturer instance. Callback methods are there if you want to do something! Duh!
@@ -61,11 +111,11 @@ public class MainActivity extends AppCompatActivity {
 
         //Create a VideoSource instance
         VideoSource videoSource = peerConnectionFactory.createVideoSource(videoCapturer.isScreencast());
-        VideoTrack localVideoTrack = peerConnectionFactory.createVideoTrack("100", videoSource);
+        localVideoTrack = peerConnectionFactory.createVideoTrack("100", videoSource);
 
         //create an AudioSource instance
         AudioSource audioSource = peerConnectionFactory.createAudioSource(constraints);
-        AudioTrack localAudioTrack = peerConnectionFactory.createAudioTrack("101", audioSource);
+        localAudioTrack = peerConnectionFactory.createAudioTrack("101", audioSource);
 
         //initalizing CameraCapturer
         SurfaceTextureHelper textureHelper = SurfaceTextureHelper.create("111", rootEglBase.getEglBaseContext());
@@ -75,16 +125,116 @@ public class MainActivity extends AppCompatActivity {
         //width,height and fps
         videoCapturer.startCapture(1000, 1000, 30);
 
+        localVideoTrack.addSink(localVideoView);
 
 
-        //create surface renderer, init it and add the renderer to the track
-        SurfaceViewRenderer videoView = findViewById(R.id.surface_view_renderer);
-        videoView.setMirror(true);
+    }
 
-        videoView.init(rootEglBase.getEglBaseContext(), null);
-        localVideoTrack.addSink(videoView);
+    private void call(){
+        start.setEnabled(false);
+        call.setEnabled(false);
+        hangup.setEnabled(true);
+
+        //we already have video and audio tracks. Now create peerconnections
+        List<PeerConnection.IceServer> iceServers = new ArrayList<>();
+
+        //create sdpConstraints
+        MediaConstraints sdpConstraints = new MediaConstraints();
+        sdpConstraints.mandatory.add(new MediaConstraints.KeyValuePair("offerToReceiveAudio", "true"));
+        sdpConstraints.mandatory.add(new MediaConstraints.KeyValuePair("offerToReceiveVideo", "true"));
+
+        //creating localPeer
+        localPeer = peerConnectionFactory.createPeerConnection(iceServers, sdpConstraints, new CustomPeerConnectionObserver("localPeerCreation") {
+            @Override
+            public void onIceCandidate(IceCandidate iceCandidate) {
+                super.onIceCandidate(iceCandidate);
+                onIceCandidateReceived(localPeer, iceCandidate);
+            }
+        });
 
 
+        //creating remotePeer
+        remotePeer = peerConnectionFactory.createPeerConnection(iceServers, sdpConstraints, new CustomPeerConnectionObserver("remotePeerCreation") {
+
+
+            //Triggered when a new ICE candidate has been found.
+            @Override
+            public void onIceCandidate(IceCandidate iceCandidate) {
+                super.onIceCandidate(iceCandidate);
+                onIceCandidateReceived(remotePeer, iceCandidate);
+            }
+
+
+            //Triggered when media is received on a new stream from remote peer.
+            @Override
+            public void onAddStream(MediaStream mediaStream) {
+                super.onAddStream(mediaStream);
+                gotRemoteStream(mediaStream);
+            }
+        });
+
+        //creating local mediastream
+        MediaStream stream = peerConnectionFactory.createLocalMediaStream("102");
+        stream.addTrack(localAudioTrack);
+        stream.addTrack(localVideoTrack);
+        localPeer.addStream(stream);
+
+        //creating Offer
+        localPeer.createOffer(new CustomSdpObserver("localCreateOffer"){
+            @Override
+            public void onCreateSuccess(SessionDescription sessionDescription) {
+                //we have localOffer. Set it as local desc for localpeer and remote desc for remote peer.
+                //try to create answer from the remote peer.
+                super.onCreateSuccess(sessionDescription);
+                localPeer.setLocalDescription(new CustomSdpObserver("localSetLocalDesc"), sessionDescription);
+                remotePeer.setRemoteDescription(new CustomSdpObserver("remoteSetRemoteDesc"), sessionDescription);
+                remotePeer.createAnswer(new CustomSdpObserver("remoteCreateOffer") {
+                    @Override
+                    public void onCreateSuccess(SessionDescription sessionDescription) {
+                        //remote answer generated. Now set it as local desc for remote peer and remote desc for local peer.
+                        super.onCreateSuccess(sessionDescription);
+                        remotePeer.setLocalDescription(new CustomSdpObserver("remoteSetLocalDesc"), sessionDescription);
+                        localPeer.setRemoteDescription(new CustomSdpObserver("localSetRemoteDesc"), sessionDescription);
+                    }
+                },new MediaConstraints());
+            }
+        },sdpConstraints);
+
+    }
+
+
+    private void hangup() {
+        localPeer.close();
+        remotePeer.close();
+        localPeer = null;
+        remotePeer = null;
+
+        start.setEnabled(true);
+        call.setEnabled(false);
+        hangup.setEnabled(false);
+    }
+
+    private void gotRemoteStream(MediaStream stream) {
+        //we have remote video stream. add to the renderer.
+        final VideoTrack videoTrack = stream.videoTracks.get(0);
+        AudioTrack audioTrack = stream.audioTracks.get(0);
+        runOnUiThread(() -> {
+            try {
+                remoteVideoView.setVisibility(View.VISIBLE);
+                videoTrack.addSink(remoteVideoView);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    public void onIceCandidateReceived(PeerConnection peer, IceCandidate iceCandidate) {
+        //we have received ice candidate. We can set it to the other peer.
+        if (peer == localPeer) {
+            remotePeer.addIceCandidate(iceCandidate);
+        } else {
+            localPeer.addIceCandidate(iceCandidate);
+        }
     }
 
 
@@ -126,5 +276,23 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return null;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.start_call: {
+                start();
+                break;
+            }
+            case R.id.init_call: {
+                call();
+                break;
+            }
+            case R.id.end_call: {
+                hangup();
+                break;
+            }
+        }
     }
 }
